9f3f48b4168f7f873133330813b4256ca83f0e13
pinkfloyda <pinkfloyda@gmail.com>
2022-04-16 23:56:29 -0500

    Update and rename 1.zero-one-knapsack.java to 1.01-knapsack.java

--- 1.01-knapsack.java
+++ 1.01-knapsack.java
@@ -1,6 +1,6 @@
 /* 01-Knapsack Problem
  *
- * Got N items and a knapsack with volumn V, each item has volumn v[i] and weight w[i],
+ * Given a knapsack with volumn V and N items where each item has volumn v[i] and weight w[i],
  * each item can only be picked once, what's the max total weight by putting items into 
  * knapsack without exceeding its volumn.
  * 
@@ -51,7 +51,7 @@ public class Main {
     /* 1) Standard DP, time O(NV), space O(NV)
      *
      * Suppose dp[i][j] is the optimal max value of puting (first) i items into a knapsack with capacity j,
-     * then dp[i][j] = max(dp[i-1][j], dp[i-1][j-v[i]] + w[i]) is optimal. Inductively, it will lead to 
+     * then dp[i][j] = max{ dp[i-1][j], dp[i-1][j-v[i]] + w[i] } is optimal. Inductively, it will lead to 
      * final optimal solution in dp[N][V], which is Dynamic Programming
      *
      * Note in the inner loop, it must loop from 1 -> V instead of v[i-1] -> V, because dp[i][1..v[i-1]] still
@@ -71,7 +71,7 @@ public class Main {
      *
      * To compute each of dp[i][0..V], it only need to access previous row, so we can just use
      * a rolling 1D array which represents both the current (after update) and previous (before update) row.
-     * But need to update the array from right-to-left: dp[j] = max(dp[j], dp[j-v[i]]+w[i])
+     * But need to update the array from right-to-left: dp[j](new-value) = max{ dp[j](old-value), dp[j-v[i]](old-value)+w[i] }
      *
      * Note in the inner loop, it can stop loop at v[i-1], not necessarily to 1, for ith row dp[1..v[i-1]]
      * are just the previous row's values, feel the difference against 2D version
@@ -88,7 +88,7 @@ public class Main {
     
     /* 3) Standard DP optimized, time O(NV), space O(NV)
      * 
-     * The right-bottom corner of the table dp[N][V] will be the final solution
+     * The right-bottom corner of the table dp[N][V] will be the final solution, so reversely
      * To compute dp[N][V], only dp[N-1][V-v[N-1]] and dp[N-1][V] will be used
      * To compute dp[N-1][V-v[N-1]], only dp[N-2][V-v[N-1]-v[N-2]] and dp[N-2][V-v[N-1]] will be used
      * 
