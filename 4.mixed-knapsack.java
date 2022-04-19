d42427c0071970ef5f424fb403951444eb9d5fbf
pinkfloyda <pinkfloyda@gmail.com>
2022-04-19 18:13:05 -0500

    Update 4.mixed-knapsack.java

--- 4.mixed-knapsack.java
+++ 4.mixed-knapsack.java
@@ -1,18 +1,21 @@
 /* Mixed-Knapsack Problem
  *
- * Given a knapsack with volumn V and N items where each item has volumn v[i], weight w[i]
- * and can be either picked once, unlimited or with quantity n[i], what's the max total weight 
- * by putting items into knapsack without exceeding its volumn.
+ * You have a knapsack with budget C and N items can be picked into knapsack,
+ * each (i-th) item has cost ci, value vi and some items can be picked once, 
+ * some can be picked unlimited times and some can be picked at most n[i] times
+ * What is the max value you can get without exceeding budget?
+ * 
  * 
  * Input:
- * First line has two integers: N and V, seperated by space
- * Each of next N lines has three integers: item's volumn, weight and quantity, sperated by space
- *   quantity == -1 means this item can be picked once
- *   quantity == 0 means this item can be picked unlimited
- *   quantity > 0 means this item can be picked with quantity
+ * First line has two integers: N and C, seperated by space
+ * Then followed by next N lines representing items:
+ *   each line has three integers: item's cost, value and count, sperated by space
+ *     count == -1 means this item can be picked once
+ *     count == 0 means this item can be picked unlimited times
+ *     count > 0 means this item can be picked at most count times
  * 
  * Output:
- * One line with one integer: max total item weight
+ * One line with one integer: max value
  *
  * Sample Input:
  *   4 5
@@ -29,23 +32,23 @@ import java.util.*;
 public class Main {
     
     static int N;
-    static int V;
+    static int C;
+    static int[] c;
     static int[] v;
-    static int[] w;
     static int[] n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-        V = sc.nextInt();
+        C = sc.nextInt();
+        c = new int[N];
         v = new int[N];
-        w = new int[N];
         n = new int[N];
 
         for(int i=0; i<N; i++) {
+            c[i] = sc.nextInt();
             v[i] = sc.nextInt();
-            w[i] = sc.nextInt();
             n[i] = sc.nextInt();
         }
         
@@ -56,32 +59,32 @@ public class Main {
  
     /* 1) Standard DP
      *
-     * Just combine solutions based on which kind of item
+     * Just combine solutions based on the kind of item picking contraints
      */
     static int knapsack() {
-        int[] dp = new int[V+1];
+        int[] dp = new int[C+1];
         for (int i=1; i<=N; i++) {
             if(n[i-1] < 0) { // using 01 knapsack solution
-                for (int j=V; j>=v[i-1]; j--) {
-                    dp[j] = Math.max(dp[j], dp[j-v[i-1]]+w[i-1]);
+                for (int j=C; j>=c[i-1]; j--) {
+                    dp[j] = Math.max(dp[j], dp[j-c[i-1]]+v[i-1]);
                 }
             } else if(n[i-1] == 0) { // using unbounded knapsack solution
-                for (int j=v[i-1]; j<=V; j++) {
-                    dp[j] = Math.max(dp[j], dp[j-v[i-1]]+w[i-1]);
+                for (int j=c[i-1]; j<=C; j++) {
+                    dp[j] = Math.max(dp[j], dp[j-c[i-1]]+v[i-1]);
                 }
             } else { // using bounded knapsack solution
                 int m = n[i-1];
-                for(int k=1, v2=v[i-1], w2=w[i-1]; m-k>0; v2*=2, w2*=2, k*=2) {
-                    for(int j=V; j>=v2; j--) {
-                        dp[j] = Math.max(dp[j], dp[j-v2]+w2);
+                for(int k=1, c2=c[i-1], v2=v[i-1]; m-k>0; c2*=2, v2*=2, k*=2) {
+                    for(int j=C; j>=c2; j--) {
+                        dp[j] = Math.max(dp[j], dp[j-c2]+v2);
                     }
-                    m -= k; 
+                    m -= k;
                 }
-                for(int j=V, v2=m*v[i-1], w2=m*w[i-1]; j>=v2; j--) {
-                    dp[j] = Math.max(dp[j], dp[j-v2]+w2);
+                for(int j=C, c2=m*c[i-1], v2=m*v[i-1]; j>=c2; j--) {
+                    dp[j] = Math.max(dp[j], dp[j-c2]+v2);
                 }
             }
         }
-        return dp[V];
+        return dp[C];
     }
 }
