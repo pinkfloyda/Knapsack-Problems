0565a1a6f46b0092ff9e6f8e8af2b4025145be09
pinkfloyda <pinkfloyda@gmail.com>
2022-04-19 18:06:01 -0500

    Update 3.bounded-knapsack.java

--- 3.bounded-knapsack.java
+++ 3.bounded-knapsack.java
@@ -1,15 +1,20 @@
 /* Bounded-Knapsack Problem
  *
- * Given a knapsack with volumn V and N items where each item has volumn v[i], weight w[i]
- * and quantity n[i], what's the max total weight by putting items into knapsack without 
- * exceeding its volumn.
+ * You have a knapsack with budget C and N items can be picked into knapsack,
+ * each (i-th) item has cost ci, value vi and can be picked at most ni times.
+ * What is the max value you can get without exceeding budget?
  * 
  * Input:
- * First line has two integers: N and V, seperated by space
- * Each of next N lines has three integers: item's volumn, weight and quantity, sperated by space
+ * First line has two integers: N and C, seperated by space
+ * Then followed by next N lines representing items:
+ *   each line has three integers: item's cost, value and count, sperated by space
  * 
  * Output:
- * One line with one integer: max total item weight
+ * One line with one integer: max value
+ *
+ * Data range:
+ * 0<N,C≤100
+ * 0<ci,vi,ni≤100
  *
  * Sample Input:
  *   4 5
@@ -26,48 +31,54 @@ import java.util.*;
 public class Main {
     
     static int N;
-    static int V;
+    static int C;
+    static int[] c;
     static int[] v;
-    static int[] w;
     static int[] n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-        V = sc.nextInt();
+        C = sc.nextInt();
+        
+        c = new int[N];
         v = new int[N];
-        w = new int[N];
         n = new int[N];
 
         for(int i=0; i<N; i++) {
+            c[i] = sc.nextInt();
             v[i] = sc.nextInt();
-            w[i] = sc.nextInt();
             n[i] = sc.nextInt();
         }
         
-        int res = knapsackBinary();
+        int res = knapsackBinaryDivide();
         
         System.out.println(res);
     }
  
-    /* 1) Standard DP, time O(VΣn[i]) ≈ O(VN^2)
+    /* 1) Standard DP using 1D rolling array, time O(CΣn[i]) ≈ O(CNN), space O(C)
      *
-     * dp[i][j] = max{ dp[i-1][j-k*v[i]]+k*w[i], 0<=k<=n[i] && k*v[i]<=j }
+     * Same thinking process as unbounded knapsack, just for ith item, can have mulitple choices
+     * with different costs and values.
+     * dp[i][j] = max{ dp[i-1][j-k*c[i]]+k*v[i], 0<=k<=n[i] && k*c[i]<=j }
      */
     static int knapsack() {
-        int[] dp = new int[V+1];
-        for (int i=1; i<=N; i++) {
-            for (int j=V; j>=v[i-1]; j--) {
-                for (int k=1, v2=v[i-1], w2=w[i-1]; v2<=j && k<=n[i-1]; v2+=v[i-1], w2+=w[i-1], k++) {
-                    dp[j] = Math.max(dp[j], dp[j-v2]+w2);
+        int[] dp = new int[C+1];
+        for(int i=1; i<=N; i++) {
+            for(int j=C; j>=c[i-1]; j--) {
+                for(int k=1, c2=c[i-1], v2=v[i-1]; c2<=j && k<=n[i-1]; c2+=c[i-1], v2+=v[i-1], k++) {
+                    dp[j] = Math.max(dp[j], dp[j-c2] + v2);
                 }
             }
         }
-        return dp[V];
+        return dp[C];
     }
     
-    /* 2) DP with binary divide, time O(VΣlogn[i]) ≈ O(VlogN)
+    /* 2) Item-Binary-Divide DP using 1D rolling array, time O(VΣlogn[i]) ≈ O(CNlogN), space O(C)
+     *
+     * Noted that this problem cannot do a simple item divide into 1,2,....k items like unbounded knapsack
+     * In that way, it may end up pick ith items more than n[i], so need below binary divide methods
      *
      * Can consider binary divide them into 1,2,4,...
      * But the tricky part is the possible max number can be represented cannot exceed n[i].
@@ -82,20 +93,20 @@ public class Main {
      *   1              2^k-1   s[i]     2^(k+1)-1
      *   A----------------B-------C----------D
      */
-    static int knapsackBinary() {
-        int[] dp = new int[V+1];
+    static int knapsackBinaryDivide() {
+        int[] dp = new int[C+1];
         for(int i=1; i<=N; i++) {
             int m = n[i-1];
-            for(int k=1, v2=v[i-1], w2=w[i-1]; m-k>0; v2*=2, w2*=2, k*=2) {
-                for(int j=V; j>=v2; j--) {
-                    dp[j] = Math.max(dp[j], dp[j-v2]+w2);
+            for(int k=1, c2=c[i-1], v2=v[i-1]; m-k>0; c2*=2, v2*=2, k*=2) {
+                for(int j=C; j>=c2; j--) {
+                    dp[j] = Math.max(dp[j], dp[j-c2] + v2);
                 }
                 m -= k; 
             }
-            for(int j=V, v2=m*v[i-1], w2=m*w[i-1]; j>=v2; j--) {
-                dp[j] = Math.max(dp[j], dp[j-v2]+w2);
+            for(int j=C, c2=m*c[i-1], v2=m*v[i-1]; j>=c2; j--) {
+                dp[j] = Math.max(dp[j], dp[j-c2] + v2);
             }
         }
-        return dp[V];
+        return dp[C];
     }
 }
