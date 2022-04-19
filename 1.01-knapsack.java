352a35b5576e384dfe01ab136fa433f5a6dd0486
pinkfloyda <pinkfloyda@gmail.com>
2022-04-19 12:37:38 -0500

    Update 1.01-knapsack.java

--- 1.01-knapsack.java
+++ 1.01-knapsack.java
@@ -1,15 +1,20 @@
 /* 01-Knapsack Problem
  *
- * Given a knapsack with volumn V and N items where each item has volumn v[i] and weight w[i],
- * each item can only be picked once, what's the max total weight by putting items into 
- * knapsack without exceeding its volumn.
+ * You have a knapsack with budget C and N items can be picked into knapsack,
+ * each (i-th) item has cost ci and value vi and each item can only be picked once.
+ * What is the max value you can get without exceeding budget?
  * 
  * Input:
- * First line has two integers: N and V, seperated by space
- * Each of next N lines has two integers: each item's volumn and weight, sperated by space
+ * First line has two integers: N and C, seperated by space
+ * Then followed by next N lines representing items:
+ *   each line has two integers: item's cost and value, sperated by space
  * 
  * Output:
- * One line with one integer: max total item weight
+ * One line with one integer: max value
+ *
+ * Data range:
+ * 0<N,C≤1000
+ * 0<ci,vi≤1000
  *
  * Sample Input:
  *   4 5
@@ -26,21 +31,22 @@ import java.util.*;
 public class Main {
     
     static int N;
-    static int V;
+    static int C;
+    static int[] c;
     static int[] v;
-    static int[] w;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         N = sc.nextInt();
-        V = sc.nextInt();
+        C = sc.nextInt();
+        
+        c = new int[N];
         v = new int[N];
-        w = new int[N];
 
         for(int i=0; i<N; i++) {
+            c[i] = sc.nextInt();
             v[i] = sc.nextInt();
-            w[i] = sc.nextInt();
         }
         
         int res = knapsack1DOptimize();
@@ -48,104 +54,114 @@ public class Main {
         System.out.println(res);
     }
 
-    /* 1) Standard DP, time O(NV), space O(NV)
+    /* 1) Standard DP using 2D array, time O(NC), space O(NC)
      *
-     * Suppose dp[i][j] is the optimal max value of puting (first) i items into a knapsack with capacity j,
-     * then dp[i][j] = max{ dp[i-1][j], dp[i-1][j-v[i]] + w[i] } is optimal. Inductively, it will lead to 
-     * final optimal solution in dp[N][V], which is Dynamic Programming
+     * dp[i][j] is the max value of picking from first i items without execeeding cost j,
+     * Suppose you have all values of dp[i-1][1..C], max values of picking from first i-1 items without
+     * execeeding each cost of 1 ~ C, then you will have option to choose ith item or not (for each cost of
+     * 1 ~ C), so dp[i][j] = max{ dp[i-1][j], dp[i-1][j-c[i]]+v[i] } (1<=j<=C)
+     * dp[N][C] will be the final answer: the max value of picking from N items without execeeding C
      *
-     * Note in the inner loop, it must loop from 1 -> V instead of v[i-1] -> V, because dp[i][1..v[i-1]] still
-     * need to be computed just in case later rows need those values.
+     * Note in the inner loop, it must loop from 1->C instead of c[i-1]->C, because dp[i][1..c[i-1]] still
+     * needed by later items, they must be assigned values of dp[i-1][1..c[i-1]]
      */
     static int knapsack() {
-        int[][] dp = new int[N+1][V+1];
+        int[][] dp = new int[N+1][C+1];
         for(int i=1; i<=N; i++) {
-            for(int j=1; j<=V; j++) {
-                dp[i][j] = Math.max(dp[i-1][j], j>=v[i-1] ? dp[i-1][j-v[i-1]]+w[i-1] : 0);
+            for(int j=1; j<=C; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (j >= c[i-1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-c[i-1]]+v[i-1]);
+                }
             }
         }
-        return dp[N][V];
+        return dp[N][C];
     }
     
-    /* 2) Standard DP with rolling 1D array, time O(NV), space O(V)
+    /* 2) Standard DP using 1D rolling array, time O(NC), space O(NC)
      *
-     * To compute each of dp[i][0..V], it only need to access previous row, so we can just use
-     * a rolling 1D array which represents both the current (after update) and previous (before update) row.
-     * But need to update the array from right-to-left: dp[j](new-value) = max{ dp[j](old-value), dp[j-v[i]](old-value)+w[i] }
+     * To compute each of dp[i][1..C], it only need to access previous row, so we can just use
+     * a rolling 1D array which represents both the current (after update) and previous (before update) rows.
+     * But need to update the array from right-to-left like below to avoid dp[j-c[i]] being overwritten:
+     * dp[j](new-value) = max{ dp[j](old-value), dp[j-c[i]](old-value)+v[i] } (1<=j<=C)
      *
-     * Note in the inner loop, it can stop loop at v[i-1], not necessarily to 1, for ith row dp[1..v[i-1]]
-     * are just the previous row's values, feel the difference against 2D version
+     * Note in the inner loop, it can stop loop at c[i-1], not necessarily to 1, for ith row dp[1..c[i-1]]
+     * are just inherit the previous values, unlike 2D array that they must be assigned values explicitly
      */
      static int knapsack1D() {
-        int[] dp = new int[V+1];
+        int[] dp = new int[C+1];
         for (int i=1; i<=N; i++) {
-            for (int j=V; j>=v[i-1]; j--) {
-                dp[j] = Math.max(dp[j], dp[j-v[i-1]]+w[i-1]);
+            for (int j=C; j>=c[i-1]; j--) {
+                dp[j] = Math.max(dp[j], dp[j-c[i-1]]+v[i-1]);
             }
         }
-        return dp[V];
+        return dp[C];
     }
     
-    /* 3) Standard DP optimized, time O(NV), space O(NV)
+    /* 3) Optimized DP using 2D array, time O(NC), space O(NC)
      * 
-     * The right-bottom corner of the table dp[N][V] will be the final solution, so reversely
-     * To compute dp[N][V], only dp[N-1][V-v[N-1]] and dp[N-1][V] will be used
-     * To compute dp[N-1][V-v[N-1]], only dp[N-2][V-v[N-1]-v[N-2]] and dp[N-2][V-v[N-1]] will be used
+     * The right-bottom corner of the table dp[N][C] will be the final answer, so think reversely:
+     * To compute dp[N][C], only dp[N-1][C-c[N-1]] and dp[N-1][C] will be used
+     * To compute dp[N-1][C-c[N-1]], only dp[N-2][C-c[N-1]-c[N-2]] and dp[N-2][C-c[N-1]] will be used
      * 
-     * So for each row i, when update, we only need to calculate starting from V-Σv[i..N]
+     * So for each row i, when update, only values starting from C-Σc[i..N] needed by later rows
      * Visually, the table is updated in a somewhat right-upper triangular fashion
      */
     static int knapsackOptimize() {
-        int[][] dp = new int[N+1][V+1];
+        int[][] dp = new int[N+1][C+1];
         int sum = 0;
         for (int i=1; i<=N; i++) {
-            sum += v[i-1];
+            sum += c[i-1];
         }
         for(int i=1; i<=N; i++) {
-            sum -= v[i-1];
-            for(int j=Math.max(V-sum, 1); j<=V; j++) {
-                dp[i][j] = Math.max(dp[i-1][j], j>=v[i-1] ? dp[i-1][j-v[i-1]]+w[i-1] : 0);
+            sum -= c[i-1];
+            for(int j=Math.max(C-sum, 1); j<=C; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (j >= c[i-1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-c[i-1]]+v[i-1]);
+                }
             }
         }
-        return dp[N][V];
+        return dp[N][C];
     }
     
-    /* 4) Standard DP with rolling 1D array optimized
-     * Same as 3) but using rolling 1D array
+    /* 4) Optimized DP using 1D rolling array, time O(NC), space O(C)
      */
     static int knapsack1DOptimize() {
-        int[] dp = new int[V+1];
+        int[] dp = new int[C+1];
         int sum = 0;
         for (int i=1; i<=N; i++) {
-            sum += v[i-1];
+            sum += c[i-1];
         }
         for (int i=1; i<=N; i++) {
-            sum -= v[i-1];
-            for (int j=V; j>=Math.max(V-sum, v[i-1]); j--) {
-                dp[j] = Math.max(dp[j], dp[j-v[i-1]]+w[i-1]);
+            sum -= c[i-1];
+            for (int j=C; j>=Math.max(C-sum, c[i-1]); j--) {
+                dp[j] = Math.max(dp[j], dp[j-c[i-1]]+v[i-1]);
             }
         }
-        return dp[V];
+        return dp[C];
     }
     
-    /* 5) Standard DP using rolling 1D array but happen to make knapsack full
+    /* 5) Standard DP using rolling 1D array but happen to meet budget (no more no less)
      *
-     * If question asks what is max total weight by putting item to make knapsack full; return -1 if not possible
-     * Similar solution like above but initially dp[0] should be 0 and dp[1..V] should all be -1. It means initially for zero items
-     * we can only make knapsack of 0 volumn full, not possible for other non-zero volumns.
-     * When update dp using 1D rolling array: dp[j] = max(dp[j], dp[j-v[i]]+w[i]), we need to check if previous value is -1 or not, if -1
-     * we cannot make knapsack full.
+     * What is max value by picking item but happen to meet budget, return -1 if not possible
+     * Similar solution like above but initially dp[0] should be 0 and dp[1..C] should all be -1. 
+     * It means initially for zero items we can meet budget of 0 without picking anything and max value is 0;
+     * not possible for other non-zero budget as we have nothing to pick from.
+     *
+     * When update 1D rolling array: dp[j] = max(dp[j], dp[j-c[i]]+v[i]), for dp[j-c[i]]+v[i] part, we need to 
+     * check if dp[j-c[i]] is -1 or not, if so we cannot pick ith item.
      */
     static int knapsackFull() {
-        int[] dp = new int[V+1];
-        for(int i=1; i<=V; i++) {
+        int[] dp = new int[C+1];
+        for(int i=1; i<=C; i++) {
             dp[i] = -1;
         }
         for (int i=1; i<=N; i++) {
-            for (int j=V; j>=v[i-1]; j--) {
-                dp[j] = Math.max(dp[j]<0 ? -1 : dp[j], dp[j-v[i-1]]<0 ? -1 : dp[j-v[i-1]]+w[i-1]);
+            for (int j=C; j>=c[i-1]; j--) {
+                dp[j] = Math.max(dp[j], dp[j-c[i-1]]<0 ? -1 : dp[j-c[i-1]]+v[i-1]);
             }
         }
-        return dp[V];
+        return dp[C];
     }
 }
