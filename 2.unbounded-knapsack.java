ccee18a52a6e68efb0464ea8b30c425b6f281da9
pinkfloyda <pinkfloyda@gmail.com>
2022-04-19 17:34:34 -0500

    Update 2.unbounded-knapsack.java

--- 2.unbounded-knapsack.java
+++ 2.unbounded-knapsack.java
@@ -1,15 +1,20 @@
 /* Unbounded-Knapsack Problem
  *
- * Given a knapsack with volumn V and N items where each item has volumn v[i] and weight w[i],
- * each item can be picked unlimited times, what's the max total weight by putting items into 
- * knapsack without exceeding its volumn.
+ * You have a knapsack with budget C and N items can be picked into knapsack,
+ * each (i-th) item has cost ci, value vi and can be picked unlimited times.
+ * What is the max value you can get without exceeding budget?
  * 
  * Input:
- * First line has two integers: N and V, seperated by space
- * Each of next N lines has two integers: each item's volumn and weight, sperated by space
+ * First line has two integers: N and C, seperated by space
+ * Then followed by next N lines representing items:
+ *   each line has two integers: item's cost and value, sperated by space
  * 
  * Output:
- * One line with one integer: max total item weight
+ * One line with one integer: max value
+ *
+ * Data range:
+ * 0<N,C≤1000
+ * 0<ci,vi≤1000
  *
  * Sample Input:
  *   4 5
@@ -26,21 +31,22 @@ import java.util.*;
 public class Main {
     
     static int N;
-    static int V;
+    static int C;
+    static int[] c;
     static int[] v;
-    static int[] w;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-        V = sc.nextInt();
+        C = sc.nextInt();
+        
+        c = new int[N];
         v = new int[N];
-        w = new int[N];
 
         for(int i=0; i<N; i++) {
+            c[i] = sc.nextInt();
             v[i] = sc.nextInt();
-            w[i] = sc.nextInt();
         }
         
         int res = knapsackOptimize1D();
@@ -48,80 +54,111 @@ public class Main {
         System.out.println(res);
     }
 
-    /* 1) Standard DP, time O(NVV), space O(NV)
+    /* 1) Standard DP using 2D array, time O(NCC), space O(NC)
      *
-     * dp[i][j]=max{ dp[i-1][V-k*v[i]]+k*w[i], 0<=k<=j/v[i]] }
+     * Similar to 01 knapsack problem, dp[i][j] stands for the max value picking from 
+     * first i items without exceeding cost j. But this time, when encounter ith item
+     * the choice is not pick vs non-pick, the choice is pick-n-times (n==0 means non-pick)
+     * 
+     * So the state transition is dp[i][j]=max{ dp[i-1][C-k*c[i]]+k*v[i] } (1<=j<=C, 0<=k<=j/c[i])
      */
     static int knapsack() {
-        int[][] dp = new int[N+1][V+1];
+        int[][] dp = new int[N+1][C+1];
         for(int i=1; i<=N; i++) {
-            for(int j=1; j<=V; j++) {
-                for(int k=0; k<=j/v[i-1]; k++) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-k*v[i-1]] + k*w[i-1]);
+            for(int j=1; j<=C; j++) {
+                for(int k=0; k<=j/c[i-1]; k++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-k*c[i-1]] + k*v[i-1]);
                 }
             }
         }
-        return dp[N][V];
+        return dp[N][C];
     }
     
-    /* 2) Standard DP with binary compaction, time O(NVlogV), space O(NV)
+    /* 2) Item-Divide DP using 2D array, time O(NCC), space O(NC)
      *
-     * For each item, we can try to break it up into m items that each item has v[i] and w[i]
-     * Then it is converted to a 01 knapsack problem, but the complexity is not changed
-     *
-     * To optimize it further, we can compact those m items into k items that each item is of volumn
-     * v[i]*2^k and weight w[i]*2^k where k always make v[i]*2^k <= V. This works because combinations
-     * of 1,2,4,....2^k can represent number from 1 to 2^(k+1)-1
-     *
-     * Noted that it is convenient to use 1D rolling array as we don't care how many items after
-     * binary divide of each item.
+     * This is an alternative thinking process, we can expand the total items by divide each
+     * item with multiple items with different costs k*c[i] and values k*v[i]
+     * Then with these expanded items, we can apply 01 knapsack problem
+     * 
+     * In implmentation, the first two loops simulate we iterate over the expanded items and 
+     * then apply 01 knapsack algorithm
+     
+     * Time comlexity not changed but worse than 1) because when we divide items, we just divide
+     * them as many as possible util single item's cost not exceed C 
      */
-    static int knapsackBinary() {
-        int[] dp = new int[V+1];
+    static int knapsackDivide() {
+        int[][] dp = new int[N+1][C+1];
         for(int i=1; i<=N; i++) {
-            for(int v2=v[i-1], w2=w[i-1]; v2<=V; v2*=2, w2*=2) {
-                for(int j=V; j>=v2; j--) {
-                    dp[j] = Math.max(dp[j], dp[j-v2] + w2);
+            for(int c2=c[i-1], v2=v[i-1]; c2<=C; c2+=c[i-1], v2+=v[i-1]) {
+                for(int j=1; j<=C; j++) {
+                    dp[i][j] = dp[i-1][j];
+                    if(j >= c2) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i-1][j-c2] + v2);
+                    }
                 }
             }
         }
-        return dp[V];
+        return dp[N][C];
     }
     
-    /* 3) DP with optimization, time O(NV), space O(NV)
+    /* 3) Item-Binary-Divide DP using 1D rolling array, time O(NClogC), space O(C)
      *
-     * The standard DP is dp[i][j]=max{ dp[i-1][V-k*v[i]]+k*w[i], 0<=k<=j/v[i]]}
-     * dp[i][j] needs to access dp[i-1][j], dp[i-1][j-v[i]], dp[i-1][j-2*v[i]], ...dp[i-1][1]
-     * dp[i][j-v[i]] needs to access        dp[i-1][j-v[i]], dp[i-1][j-2*v[i]], ...dp[i-1][1]
-     * To compute dp[i][j], we just need to access dp[i-1][j] and dp[i][j-v[i]]
-     * So the new DP transition is dp[i][j]=max{ dp[i-1][j], dp[i][j-v[i]]+w[i] }
+     * Continued from above item-divide thinking process, but we can divide item in a smarter
+     * way: divide item into k items that each item has cost (2^k)*c[i] and value (2^k)*v[i]
+     * where k starts from zero until the max value that make (2^k)*c[i] <= C
+     *
+     * This works because combinations of 1,2,4,....2^k can represent any number from 1 to 2^(k+1)-1
+     * And time complexity improves as the total number of expanded items to iterate is NlogC
+     */
+    static int knapsackBinaryDivide() {
+        int[] dp = new int[C+1];
+        for(int i=1; i<=N; i++) {
+            for(int c2=c[i-1], v2=v[i-1]; c2<=C; c2*=2, v2*=2) {
+                for(int j=C; j>=c2; j--) {
+                    dp[j] = Math.max(dp[j], dp[j-c2] + v2);
+                }
+            }
+        }
+        return dp[C];
+    }
+    
+    /* 4) Optimized DP using 2D array, time O(NC), space O(NC)
+     *
+     * In standard DP, state transition is dp[i][j]=max{ dp[i-1][j-k*c[i]]+k*v[i], 0<=k<=j/c[i]]}
+     * dp[i][j] needs to access dp[i-1][j], dp[i-1][j-c[i]], dp[i-1][j-2*c[i]], ...dp[i-1][1]
+     * dp[i][j-c[i]] needs to access        dp[i-1][j-c[i]], dp[i-1][j-2*c[i]], ...dp[i-1][1]
+     * To compute dp[i][j], we just need to access dp[i-1][j] and dp[i][j-c[i]]
+     * So the new DP transition is dp[i][j]=max{ dp[i-1][j], dp[i][j-c[i]]+v[i] }
      */
     static int knapsackOptimize() {
-        int[][] dp = new int[N+1][V+1];
+        int[][] dp = new int[N+1][C+1];
         for (int i=1; i<=N; i++) {
-            for (int j=1; j<=V; j++) {
-                dp[i][j] = Math.max(dp[i-1][j], v[i-1]<=j ? dp[i][j-v[i-1]] + w[i-1] : 0);
+            for (int j=1; j<=C; j++) {
+                dp[i][j] = dp[i-1][j];
+                if(j >= c[i-1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j-c[i-1]] + v[i-1]);
+                }
             }
         }
-        return dp[N][V];
+        return dp[N][C];
     }
     
-    /* 4) DP with optimization using rolling 1D array
+    /* 5) Optimized DP using 1D rolling array, time O(NC), space O(C)
      * 
-     * Each row only access previous row and current row, so can just 1D rolling array
-     * But unlike 01 knapsack, it must update from left to right, because the first updated dp values
+     * Each row only access previous and current row, so can just 1D rolling array
+     * But unlike 01 knapsack, it must update from left to right, because the former updated dp values
      * are needed by later ones. 
-     * dp[j](new-value) = max{ dp[j](old-value), dp[j-v[i]](new-value)+w[i] }
+     * dp[j](new-value) = max{ dp[j](old-value), dp[j-c[i]](new-value)+v[i] }
      *
-     * Noted that the inner loop need to start from v[i-1] because former ones just old values
+     * Noted that the inner loop need to start from c[i-1] so those less than c[i-1] just inherit the old values
      */
     static int knapsackOptimize1D() {
-        int[] dp = new int[V+1];
+        int[] dp = new int[C+1];
         for (int i=1; i<=N; i++) {
-            for (int j=v[i-1]; j<=V; j++) {
-                dp[j] = Math.max(dp[j], dp[j-v[i-1]]+w[i-1]);
+            for (int j=c[i-1]; j<=C; j++) {
+                dp[j] = Math.max(dp[j], dp[j-c[i-1]] + v[i-1]);
             }
         }
-        return dp[V];
+        return dp[C];
     }
 }
